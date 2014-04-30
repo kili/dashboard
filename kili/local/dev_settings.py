37dbc4a394b504ca23a9751e70d001ae6b5697c7
replay <mauro.stettler@gmail.com>
2014-04-30 14:01:02 +0300

    updated dev_settings to use ihub environment

--- kili/local/dev_settings.py
+++ kili/local/dev_settings.py
@@ -1,8 +1,8 @@
 import os
 from kili.settings import *
 
-KEYSTONE_TOKEN = "JbztrK_H2,yO0jJ4.Vje"
-KEYSTONE_URL = "http://10.186.43.2:35357/v2.0"
+KEYSTONE_TOKEN = "HWXvI5jbn3pln5m1u0Iw"
+KEYSTONE_URL = "http://10.186.43.12:35357/v2.0"
 
 BROKER_URL = 'amqp://horizon_dashboard:iamapassword@127.0.0.1//horizon_dashboard'
         
@@ -12,7 +12,7 @@ DATABASES = {
         'NAME': 'horizon_dashboard',
         'USER': 'horizon',
         'PASSWORD': 'vSzjLZT3YmdzNL1k7OuJ',
-        'HOST': '10.186.43.2',
+        'HOST': '10.186.43.12',
         'PORT': '3306',
     },
 }   
@@ -27,11 +27,11 @@ MIDDLEWARE_CLASSES += (
     'debug_toolbar.middleware.DebugToolbarMiddleware',
 )
 
-OPENSTACK_HOST = "10.186.43.2"
+OPENSTACK_HOST = "10.186.43.12"
 OPENSTACK_KEYSTONE_URL = "http://{}:5000/v2.0".format(OPENSTACK_HOST)
-        
+
 EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
-    
+
 COMPRESS_ENABLED = False
 
 DEBUG = True
