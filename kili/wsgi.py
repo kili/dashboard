2525d6aab06719e52a6df33e1593c393983500e5
replay <mauro.stettler@gmail.com>
2014-05-01 08:34:03 +0300

    fix wsgi to load newrelic

--- kili/wsgi.py
+++ kili/wsgi.py
@@ -11,6 +11,7 @@ import os
 import sys
 import django.core.handlers.wsgi
 from django.conf import settings
+import newrelic.agent
 
 # Add this file path to sys.path in order to import settings
 sys.path.insert(0, os.path.join(os.path.dirname(
@@ -20,4 +21,7 @@ sys.stdout = sys.stderr
 
 DEBUG = False
 
+newrelic.agent.initialize(settings.NEWRELIC_CONFIG_FILE)
+
 application = django.core.handlers.wsgi.WSGIHandler()
+application = newrelic.agent.wsgi_application()(application)
