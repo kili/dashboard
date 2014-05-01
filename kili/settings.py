2730137a978fc05af10c2aaed33b0601d31f6480
replay <mauro.stettler@gmail.com>
2014-05-01 16:10:56 +0300

    update settings.py to match icehouse

--- kili/settings.py
+++ kili/settings.py
@@ -1,41 +1,64 @@
-"""
-Django settings for kili project.
+# vim: tabstop=4 shiftwidth=4 softtabstop=4
 
-For more information on this file, see
-https://docs.djangoproject.com/en/1.6/topics/settings/
+# Copyright 2012 United States Government as represented by the
+# Administrator of the National Aeronautics and Space Administration.
+# All Rights Reserved.
+#
+# Copyright 2012 Nebula, Inc.
+#
+#    Licensed under the Apache License, Version 2.0 (the "License"); you may
+#    not use this file except in compliance with the License. You may obtain
+#    a copy of the License at
+#
+#         http://www.apache.org/licenses/LICENSE-2.0
+#
+#    Unless required by applicable law or agreed to in writing, software
+#    distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+#    WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+#    License for the specific language governing permissions and limitations
+#    under the License.
 
-For the full list of settings and their values, see
-https://docs.djangoproject.com/en/1.6/ref/settings/
-"""
-
-# Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 import logging
 import os
 import sys
-BASE_DIR = os.path.dirname(os.path.dirname(__file__))
+import warnings
 
-from openstack_dashboard import exceptions
+# Load the pluggable dashboard settings
 import openstack_dashboard.enabled
 import openstack_dashboard.local.enabled
 from openstack_dashboard.utils import settings
+from django.utils.translation import ugettext_lazy as _
+from openstack_dashboard import exceptions
 from kombu import Exchange, Queue
 
-DATABASES = { 
-        'default': {
-                    'ENGINE': 'django.db.backends.mysql',
-                    'NAME': 'horizon_dashboard',
-                    'USER': 'horizon',
-                    'PASSWORD': 'vSzjLZT3YmdzNL1k7OuJ',
-                    'HOST': '10.186.43.2',
-                    'PORT': '3306',
-                },  
-}
+warnings.formatwarning = lambda message, category, *args, **kwargs: \
+    '%s: %s' % (category.__name__, message)
 
+ROOT_PATH = os.path.dirname(os.path.abspath(__file__))
+BIN_DIR = os.path.abspath(os.path.join(ROOT_PATH, '..', 'bin'))
 
-# Quick-start development settings - unsuitable for production
-# See https://docs.djangoproject.com/en/1.6/howto/deployment/checklist/
+if ROOT_PATH not in sys.path:
+    sys.path.append(ROOT_PATH)
 
-# SECURITY WARNING: keep the secret key used in production secret!
+DEBUG = False
+TEMPLATE_DEBUG = DEBUG
+
+SITE_BRANDING = 'OpenStack Dashboard'
+
+LOGIN_URL = '/auth/login/'
+LOGOUT_URL = '/auth/logout/'
+# LOGIN_REDIRECT_URL can be used as an alternative for
+# HORIZON_CONFIG.user_home, if user_home is not set.
+# Do not set it to '/home/', as this will cause circular redirect loop
+LOGIN_REDIRECT_URL = '/'
+
+MEDIA_ROOT = os.path.abspath(os.path.join(ROOT_PATH, '..', 'media'))
+MEDIA_URL = '/media/'
+STATIC_ROOT = os.path.abspath(os.path.join(ROOT_PATH, '..', 'static'))
+STATIC_URL = '/static/'
+
+ROOT_URLCONF = 'kili.urls'
+
 SECRET_KEY = '_8!hwo0@xfkkz15e%+nh4f0v7ra7)jucipgt9)o(3s16v=^tk%'
 
 HORIZON_CONFIG = {
@@ -54,33 +77,61 @@ HORIZON_CONFIG = {
                    'unauthorized': exceptions.UNAUTHORIZED},
 }
 
-ROOT_PATH = os.path.dirname(os.path.abspath(__file__))
-BIN_DIR = os.path.abspath(os.path.join(ROOT_PATH, '..', 'bin'))
+# Set to True to allow users to upload images to glance via Horizon server.
+# When enabled, a file form field will appear on the create image form.
+# See documentation for deployment considerations.
+HORIZON_IMAGES_ALLOW_UPLOAD = True
 
-if ROOT_PATH not in sys.path:
-    sys.path.append(ROOT_PATH)
-
-DEBUG = False 
-TEMPLATE_DEBUG = DEBUG
-
-SITE_BRANDING = 'OpenStack Dashboard'
-
-LOGIN_URL = '/auth/login/'
-LOGOUT_URL = '/auth/logout/'
-# LOGIN_REDIRECT_URL can be used as an alternative for
-# HORIZON_CONFIG.user_home, if user_home is not set.
-# Do not set it to '/home/', as this will cause circular redirect loop
-LOGIN_REDIRECT_URL = '/'
-
-MEDIA_ROOT = os.path.abspath(os.path.join(ROOT_PATH, '..', 'media'))
-MEDIA_URL = '/media/'
-STATIC_ROOT = os.path.abspath(os.path.join(ROOT_PATH, '..', 'static'))
-STATIC_URL = '/static/'
-
-COMPRESS_OFFLINE_CONTEXT = {
-    'STATIC_URL': STATIC_URL,
-    'HORIZON_CONFIG': HORIZON_CONFIG
+# The OPENSTACK_IMAGE_BACKEND settings can be used to customize features
+# in the OpenStack Dashboard related to the Image service, such as the list
+# of supported image formats.
+OPENSTACK_IMAGE_BACKEND = {
+    'image_formats': [
+        ('', ''),
+        ('aki', _('AKI - Amazon Kernel Image')),
+        ('ami', _('AMI - Amazon Machine Image')),
+        ('ari', _('ARI - Amazon Ramdisk Image')),
+        ('iso', _('ISO - Optical Disk Image')),
+        ('qcow2', _('QCOW2 - QEMU Emulator')),
+        ('raw', _('Raw')),
+        ('vdi', _('VDI')),
+        ('vhd', _('VHD')),
+        ('vmdk', _('VMDK'))
+    ]
 }
+
+MIDDLEWARE_CLASSES = (
+    'django.middleware.common.CommonMiddleware',
+    'django.middleware.csrf.CsrfViewMiddleware',
+    'django.contrib.sessions.middleware.SessionMiddleware',
+    'django.contrib.auth.middleware.AuthenticationMiddleware',
+    'django.contrib.messages.middleware.MessageMiddleware',
+    'horizon.middleware.HorizonMiddleware',
+    'django.middleware.doc.XViewMiddleware',
+    'django.middleware.locale.LocaleMiddleware',
+    'django.middleware.clickjacking.XFrameOptionsMiddleware',
+)
+
+TEMPLATE_CONTEXT_PROCESSORS = (
+    'django.core.context_processors.debug',
+    'django.core.context_processors.i18n',
+    'django.core.context_processors.request',
+    'django.core.context_processors.media',
+    'django.core.context_processors.static',
+    'django.contrib.messages.context_processors.messages',
+    'horizon.context_processors.horizon',
+    'openstack_dashboard.context_processors.openstack',
+)
+
+TEMPLATE_LOADERS = (
+    'django.template.loaders.filesystem.Loader',
+    'django.template.loaders.app_directories.Loader',
+    'horizon.loaders.TemplateLoader'
+)
+
+TEMPLATE_DIRS = (
+    os.path.join(ROOT_PATH, 'templates'),
+)
 
 STATICFILES_FINDERS = (
     'compressor.finders.CompressorFinder',
@@ -100,11 +151,7 @@ COMPRESS_OUTPUT_DIR = 'dashboard'
 COMPRESS_CSS_HASHING_METHOD = 'hash'
 COMPRESS_PARSER = 'compressor.parser.HtmlParser'
 
-ALLOWED_HOSTS = [ '*', ]
-
-# Application definition
-
-INSTALLED_APPS = (
+INSTALLED_APPS = [
     'kili',
     'openstack_dashboard',
     'django.contrib.contenttypes',
@@ -112,7 +159,6 @@ INSTALLED_APPS = (
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'django.contrib.sites',
     'django.contrib.humanize',
     'compressor',
     'horizon',
@@ -122,15 +168,21 @@ INSTALLED_APPS = (
     'allauth.account',
     'south',
     'async',
-)
+]
 
 TEST_RUNNER = 'django_nose.NoseTestSuiteRunner'
 AUTHENTICATION_BACKENDS = ('openstack_auth.backend.KeystoneBackend',)
 MESSAGE_STORAGE = 'django.contrib.messages.storage.cookie.CookieStorage'
 
-SESSION_ENGINE = 'django.contrib.sessions.backends.cached_db'
+SESSION_ENGINE = 'django.contrib.sessions.backends.signed_cookies'
 SESSION_COOKIE_HTTPONLY = True
+SESSION_EXPIRE_AT_BROWSER_CLOSE = True
+SESSION_COOKIE_SECURE = False
 SESSION_TIMEOUT = 1800
+
+# When using cookie-based sessions, log error when the session cookie exceeds
+# the following size (common browsers drop cookies above a certain size):
+SESSION_COOKIE_MAX_SIZE = 4093
 
 # when doing upgrades, it may be wise to stick to PickleSerializer
 # TODO(mrunge): remove after Icehouse
@@ -168,71 +220,24 @@ POLICY_FILES = {
     'image': 'glance_policy.json',
     'orchestration': 'heat_policy.json',
 }
+
 LOCAL_PATH = None
 
-MIDDLEWARE_CLASSES = ( 
-        'django.middleware.cache.UpdateCacheMiddleware',
-        'django.middleware.common.CommonMiddleware',
-        'django.middleware.csrf.CsrfViewMiddleware',
-        'django.contrib.sessions.middleware.SessionMiddleware',
-        'django.contrib.auth.middleware.AuthenticationMiddleware',
-        'django.contrib.messages.middleware.MessageMiddleware',
-        'horizon.middleware.HorizonMiddleware',
-        'django.middleware.doc.XViewMiddleware',
-        'django.middleware.locale.LocaleMiddleware',
-        'django.middleware.clickjacking.XFrameOptionsMiddleware',
-        'django.middleware.cache.FetchFromCacheMiddleware',
-)
 
-CACHES = { 
-    'default': {
-        'BACKEND': 'django.core.cache.backends.memcached.MemcachedCache',
-        'LOCATION': '127.0.0.1:11211',
-    }   
+INSTALLED_APPS = list(INSTALLED_APPS)  # Make sure it's mutable
+settings.update_dashboards([
+    openstack_dashboard.enabled,
+    openstack_dashboard.local.enabled,
+], HORIZON_CONFIG, INSTALLED_APPS)
+
+# Add HORIZON_CONFIG to the context information for offline compression
+COMPRESS_OFFLINE_CONTEXT = {
+    'STATIC_URL': STATIC_URL,
+    'HORIZON_CONFIG': HORIZON_CONFIG
 }
 
-TEMPLATE_CONTEXT_PROCESSORS = (
-    'django.core.context_processors.debug',
-    'django.core.context_processors.i18n',
-    'django.core.context_processors.request',
-    'django.core.context_processors.media',
-    'django.core.context_processors.static',
-    'django.contrib.messages.context_processors.messages',
-    'horizon.context_processors.horizon',
-    'openstack_dashboard.context_processors.openstack',
-)
-
-TEMPLATE_LOADERS = (
-    'django.template.loaders.filesystem.Loader',
-    'django.template.loaders.app_directories.Loader',
-    'horizon.loaders.TemplateLoader'
-)
-
-TEMPLATE_DIRS = (
-    os.path.join(ROOT_PATH, 'templates'),
-)
-
-ROOT_URLCONF = 'kili.urls'
-
-WSGI_APPLICATION = 'kili.wsgi.application'
-
-# Internationalization
-# https://docs.djangoproject.com/en/1.6/topics/i18n/
-
-LANGUAGE_CODE = 'en-us'
-
-TIME_ZONE = 'UTC'
-
-USE_I18N = True
-
-USE_L10N = True
-
-USE_TZ = True
-
-# Static files (CSS, JavaScript, Images)
-# https://docs.djangoproject.com/en/1.6/howto/static-files/
-
-STATIC_ROOT = os.path.abspath(os.path.join(os.path.dirname(__file__), '..', 'static'))
+if DEBUG:
+    logging.basicConfig(level=logging.DEBUG)
 
 DEFAULT_FROM_EMAIL = "Kili <help@kili.io>"
 
@@ -268,38 +273,17 @@ EMAIL_CONFIRMATION_REDIRECT = "/horizon"
 COMPRESS_OFFLINE = False
 ACCOUNT_DEFAULT_HTTP_PROTOCOL = 'HTTPS'
 
-COMPRESS_OFFLINE_CONTEXT = {
-        'STATIC_URL': STATIC_URL,
-        'HORIZON_CONFIG': HORIZON_CONFIG
+OPENSTACK_NEUTRON_NETWORK = {
+    'enable_lb': False,
+    'enable_firewall': False,
+    'enable_quotas': True,
+    'enable_vpn': False,
+    # The profile_support option is used to detect if an external router can be
+    # configured via the dashboard. When using specific plugins the
+    # profile_support can be turned on if needed.
+    'profile_support': None,
+    #'profile_support': 'cisco',
 }
-
-OPENSTACK_HOST = "10.186.43.2"
-OPENSTACK_KEYSTONE_URL = "http://{}:5000/v2.0".format(OPENSTACK_HOST)
-OPENSTACK_KEYSTONE_DEFAULT_ROLE = "_member_"
-
-COMPRESS_ENABLED = True
-
-EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
-
-KEYSTONE_TOKEN = "JbztrK_H2,yO0jJ4.Vje"
-KEYSTONE_DEFAULT_GROUP = "admin"
-
-#OPENSTACK_KEYSTONE_MULTIDOMAIN_SUPPORT = True
-
-
-OPENSTACK_API_VERSION = {
-    'identity': 2.0,
-    'volume': 2,
-}
-
-INSTALLED_APPS = list(INSTALLED_APPS)  # Make sure it's mutable
-settings.update_dashboards([
-        openstack_dashboard.enabled,
-        openstack_dashboard.local.enabled,
-], HORIZON_CONFIG, INSTALLED_APPS)
-
-if DEBUG:
-    logging.basicConfig(level=logging.DEBUG)
 
 LOGGING = {
     'version': 1,
@@ -410,4 +394,120 @@ LOGGING = {
             'propagate': False,
         },
     }
+}
+
+SECURITY_GROUP_RULES = {
+    'all_tcp': {
+        'name': 'ALL TCP',
+        'ip_protocol': 'tcp',
+        'from_port': '1',
+        'to_port': '65535',
+    },
+    'all_udp': {
+        'name': 'ALL UDP',
+        'ip_protocol': 'udp',
+        'from_port': '1',
+        'to_port': '65535',
+    },
+    'all_icmp': {
+        'name': 'ALL ICMP',
+        'ip_protocol': 'icmp',
+        'from_port': '-1',
+        'to_port': '-1',
+    },
+    'ssh': {
+        'name': 'SSH',
+        'ip_protocol': 'tcp',
+        'from_port': '22',
+        'to_port': '22',
+    },
+    'smtp': {
+        'name': 'SMTP',
+        'ip_protocol': 'tcp',
+        'from_port': '25',
+        'to_port': '25',
+    },
+    'dns': {
+        'name': 'DNS',
+        'ip_protocol': 'tcp',
+        'from_port': '53',
+        'to_port': '53',
+    },
+    'http': {
+        'name': 'HTTP',
+        'ip_protocol': 'tcp',
+        'from_port': '80',
+        'to_port': '80',
+    },
+    'pop3': {
+        'name': 'POP3',
+        'ip_protocol': 'tcp',
+        'from_port': '110',
+        'to_port': '110',
+    },
+    'imap': {
+        'name': 'IMAP',
+        'ip_protocol': 'tcp',
+        'from_port': '143',
+        'to_port': '143',
+    },
+    'ldap': {
+        'name': 'LDAP',
+        'ip_protocol': 'tcp',
+        'from_port': '389',
+        'to_port': '389',
+    },
+    'https': {
+        'name': 'HTTPS',
+        'ip_protocol': 'tcp',
+        'from_port': '443',
+        'to_port': '443',
+    },
+    'smtps': {
+        'name': 'SMTPS',
+        'ip_protocol': 'tcp',
+        'from_port': '465',
+        'to_port': '465',
+    },
+    'imaps': {
+        'name': 'IMAPS',
+        'ip_protocol': 'tcp',
+        'from_port': '993',
+        'to_port': '993',
+    },
+    'pop3s': {
+        'name': 'POP3S',
+        'ip_protocol': 'tcp',
+        'from_port': '995',
+        'to_port': '995',
+    },
+    'ms_sql': {
+        'name': 'MS SQL',
+        'ip_protocol': 'tcp',
+        'from_port': '1433',
+        'to_port': '1433',
+    },
+    'mysql': {
+        'name': 'MYSQL',
+        'ip_protocol': 'tcp',
+        'from_port': '3306',
+        'to_port': '3306',
+    },
+    'rdp': {
+        'name': 'RDP',
+        'ip_protocol': 'tcp',
+        'from_port': '3389',
+        'to_port': '3389',
+    },
+}
+
+FLAVOR_EXTRA_KEYS = {
+    'flavor_keys': [
+        ('quota:read_bytes_sec', _('Quota: Read bytes')),
+        ('quota:write_bytes_sec', _('Quota: Write bytes')),
+        ('quota:cpu_quota', _('Quota: CPU')),
+        ('quota:cpu_period', _('Quota: CPU period')),
+        ('quota:inbound_average', _('Quota: Inbound average')),
+        ('quota:outbound_average', _('Quota: Outbound average')),
+    ]
 }
