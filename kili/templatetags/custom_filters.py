5fde473a618716b572d8c1b7ec9d744b0a1a6794
James Nzomo <kazikubwa@gmail.com>
2014-05-02 19:24:34 +0300

    Change login form label from 'User Name' to 'Email'

---
+++ kili/templatetags/custom_filters.py
@@ -0,0 +1,10 @@
+import re
+from django import template
+
+register = template.Library()
+
+
+@register.filter
+def replace(value, args):
+    """Removes all values of arg from the given string"""
+    return value.replace(args.split(",")[0], args.split(",")[1])
