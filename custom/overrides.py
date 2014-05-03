913c36b1b2d9faef52392c6a23d7ad42086ab9f5
replay <mauro.stettler@gmail.com>
2014-05-03 17:19:49 +0300

    making pep8 happy

--- custom/overrides.py
+++ custom/overrides.py
@@ -1,8 +1,9 @@
 from openstack_dashboard.dashboards.project.instances.views \
     import LaunchInstanceView
-from openstack_dashboard.dashboards.project.instances.workflows.create_instance \
-    import *
+from openstack_dashboard.dashboards.project.instances.workflows. \
+    create_instance import *
 from custom.dashboards.project.instances.workflows.create_instance import *
+
 
 LaunchInstanceView.workflow_class.default_steps = (SelectProjectUser,
                                                    SetInstanceDetails,
