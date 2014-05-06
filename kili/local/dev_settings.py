e0573b76270ebdc50fcdd6cfa006f9fa9e193c09
replay <mauro.stettler@gmail.com>
2014-05-06 16:03:51 +0300

    fix flake8 in dev_settings.py

--- kili/local/dev_settings.py
+++ kili/local/dev_settings.py
@@ -1,34 +1,34 @@
-import os
-from kili.settings import *
+from kili.settings import *  # noqa
 
 KEYSTONE_TOKEN = "HWXvI5jbn3pln5m1u0Iw"
-KEYSTONE_URL = "http://10.0.1.222:35357/v2.0"
+KEYSTONE_URL = "http://10.186.43.12:35357/v2.0"
 
-BROKER_URL = 'amqp://horizon_dashboard:iamapassword@127.0.0.1//horizon_dashboard'
-        
-DATABASES = { 
+BROKER_URL = \
+    'amqp://horizon_dashboard:iamapassword@127.0.0.1//horizon_dashboard'
+
+DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql',
         'NAME': 'horizon_dashboard',
         'USER': 'horizon',
         'PASSWORD': 'vSzjLZT3YmdzNL1k7OuJ',
-        'HOST': '127.0.0.1',
+        'HOST': '10.186.43.12',
         'PORT': '3306',
     },
-}   
+}
 
 INSTALLED_APPS += (
     'debug_toolbar',
 )
 
-INTERNAL_IPS = ( '127.0.0.1', )
+INTERNAL_IPS = ('127.0.0.1', )
 
 MIDDLEWARE_CLASSES += (
     'debug_toolbar.middleware.DebugToolbarMiddleware',
 )
 
-OPENSTACK_HOST = "10.0.1.223"
-OPENSTACK_KEYSTONE_URL = "http://{}:80/keystone/v2.0".format(OPENSTACK_HOST)
+OPENSTACK_HOST = "10.186.43.12"
+OPENSTACK_KEYSTONE_URL = "http://{}:5000/v2.0".format(OPENSTACK_HOST)
 
 EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
 
@@ -36,9 +36,8 @@ COMPRESS_ENABLED = False
 
 DEBUG = True
 
-ENV_NAME = "dev" 
+ENV_NAME = "dev"
 
 DEBUG_TOOLBAR_PATCH_SETTINGS = False
 
 OPENSTACK_SSL_NO_VERIFY = True
-
