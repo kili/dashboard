0b4d21161a99587599a5ee0d373b1497827ac863
replay <mauro.stettler@gmail.com>
2014-05-29 14:58:02 +0300

    more testsg

--- accounting/tests.py
+++ accounting/tests.py
@@ -22,23 +22,11 @@ class SimpleTest(test.TestCase):
         self.promotions_account = self.account_manager.get_account(
             settings.ACCOUNTING_PROMOTIONS_ACCOUNT)
 
-    def test_get_book(self):
-        self.assertEqual(
-            self.book_manager.get_book(
-                settings.ACCOUNTING_BOOKS.keys()[0]).description,
-            settings.ACCOUNTING_BOOKS.values()[0])
-
-    def test_account_charging(self):
+    def test_receive_user_payment(self):
         self.user_transactions.receive_user_payment(
             self.user_id, self.asset_source_account.name, 30)
         self.assertEqual(self.user_account.balance(), 30)
         self.assertEqual(self.asset_source_account.balance(), 30)
-
-    def test_account_positivity(self):
-        self.assertEqual(self.user_account.positive_credit, True)
-        self.assertEqual(self.revenue_account.positive_credit, True)
-        self.assertEqual(self.asset_source_account.positive_credit, False)
-        self.assertEqual(self.promotions_account.positive_credit, False)
 
     def test_consume_user_money(self):
         self.user_transactions.receive_user_payment(
@@ -47,22 +35,47 @@ class SimpleTest(test.TestCase):
         self.assertEqual(self.user_account.balance(), 50)
         self.assertEqual(self.revenue_account.balance(), 100)
 
+    def test_grant_user_promotion(self):
+        self.user_transactions.grant_user_promotion(
+            self.user_id, 100, 'free money!')
+        self.assertEqual(self.promotions_account.balance(), 100)
+        self.assertEqual(self.user_account.balance(), 100)
+
+    def test_promotion_message(self):
+        self.user_transactions.grant_user_promotion(
+            self.user_id, 100, "free money!")
+        self.assertEqual(
+            self.transaction_history.get_user_account_transaction_history(
+                self.user_id)[0]["description"], "free money!")
+
     def test_transaction_history(self):
+        self.user_transactions.receive_user_payment(
+            self.user_id, self.asset_source_account.name, 150)
+        self.user_transactions.consume_user_money(self.user_id, 100, 'cpu')
+        self.assertEqual(
+            self.transaction_history.get_account_transaction_history(
+                settings.ACCOUNTING_USER_ACCOUNT_FORMAT["format"].format(
+                    self.user_id))[0]["amount"], -150)
+        self.assertEqual(
+            self.transaction_history.get_account_transaction_history(
+                settings.ACCOUNTING_USER_ACCOUNT_FORMAT["format"].format(
+                    self.user_id), user_values=True)[0]["amount"], 150)
+        self.assertEqual(
+            self.transaction_history.get_account_transaction_history(
+                settings.ACCOUNTING_USER_ACCOUNT_FORMAT["format"].format(
+                    self.user_id))[1]["amount"], 100)
+        self.assertEqual(
+            self.transaction_history.get_account_transaction_history(
+                settings.ACCOUNTING_USER_ACCOUNT_FORMAT["format"].format(
+                    self.user_id))[1]["description"], "resource usage: cpu")
+
+    def test_user_transaction_history(self):
         self.user_transactions.receive_user_payment(
             self.user_id, self.asset_source_account.name, 150)
         self.user_transactions.consume_user_money(self.user_id, 100, 'cpu')
         self.assertEqual(
             self.transaction_history.get_user_account_transaction_history(
                 self.user_id)[0]["amount"], -150)
-        self.assertEqual(
-            self.transaction_history.get_user_account_transaction_history(
-                self.user_id, user_values=True)[0]["amount"], 150)
-        self.assertEqual(
-            self.transaction_history.get_user_account_transaction_history(
-                self.user_id)[1]["amount"], 100)
-        self.assertEqual(
-            self.transaction_history.get_user_account_transaction_history(
-                self.user_id)[1]["description"], "resource usage: cpu")
 
     def test_transaction_history_pagination(self):
         self.user_transactions.receive_user_payment(
@@ -81,5 +94,46 @@ class SimpleTest(test.TestCase):
             )[0]["amount"], -2)
         self.assertEqual(
             self.transaction_history.get_user_account_transaction_history(
+                self.user_id, paginate=True, coords={"begin": 1, "end": 2}
+            )[-1:][0]["amount"], -2)
+        self.assertEqual(
+            self.transaction_history.get_user_account_transaction_history(
                 self.user_id, paginate=True, coords={"begin": 4, "end": 5}
             )[0]["amount"], -5)
+        self.assertEqual(
+            self.transaction_history.get_user_account_transaction_history(
+                self.user_id, paginate=True, coords={"begin": 1, "end": 5}
+            )[-1:][0]["amount"], -5)
+
+    def test_get_book_by_currency(self):
+        self.assertEqual(
+            self.book_manager.get_book(
+                settings.ACCOUNTING_BOOKS.keys()[0]).description,
+            settings.ACCOUNTING_BOOKS.values()[0])
+
+    def test_account_name_validation(self):
+        valid_account_names = settings.ACCOUNTING_ASSET_SOURCES[:]
+        valid_account_names.append(settings.ACCOUNTING_PROMOTIONS_ACCOUNT)
+        valid_account_names.append(settings.ACCOUNTING_REVENUE_ACCOUNT)
+
+        for name in valid_account_names:
+            self.assertEqual(self.account_manager.name_is_valid(name),
+                             True)
+        self.assertEqual(self.account_manager.name_is_valid(
+            settings.ACCOUNTING_USER_ACCOUNT_FORMAT["format"].format(
+                self.user_id)), True)
+        self.assertEqual(self.account_manager.name_is_valid(
+            settings.ACCOUNTING_USER_ACCOUNT_FORMAT["format"].format(
+                self.user_id)[:-1]), False)
+
+    def test_format_user_account(self):
+        self.assertEqual(
+            self.account_manager.format_user_account(self.user_id),
+            settings.ACCOUNTING_USER_ACCOUNT_FORMAT["format"].format(
+                self.user_id))
+
+    def test_account_positivity(self):
+        self.assertEqual(self.user_account.positive_credit, True)
+        self.assertEqual(self.revenue_account.positive_credit, True)
+        self.assertEqual(self.asset_source_account.positive_credit, False)
+        self.assertEqual(self.promotions_account.positive_credit, False)
