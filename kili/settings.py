337b4091c045483ef4e92a6e48391b24c61f346f
replay <mauro.stettler@gmail.com>
2014-06-06 16:04:24 +0300

    configure the resource_pricing

--- kili/settings.py
+++ kili/settings.py
@@ -181,6 +181,8 @@ INSTALLED_APPS = [
     'async',
     'crispy_forms',
     'resource_pricing',
+    'resource_pricing.calculators.instance',
+    'resource_pricing.calculators.volume',
 ]
 
 TEST_RUNNER = 'django_nose.NoseTestSuiteRunner'
@@ -545,3 +547,12 @@ ACCOUNTING_REVENUE_ACCOUNT = "REVENUE"
 ACCOUNTING_USER_ACCOUNT_FORMAT = {"regex": "USER_[a-f0-9]{32}",
                                   "format": "USER_{0}"}
 
+
+BILLABLE_RESOURCE_TYPES = {
+    'instance': {'id': 0,
+                 'price_calculator': 'resource_pricing.calculators.instance'},
+    'volume': {'id': 1,
+               'price_calculator': 'resource_pricing.calculators.volume'},
+    'network': {'id': 2,
+                'price_calculator': 'resource_pricing.calculators.network'},
+}
