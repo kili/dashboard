631e69ce852772cdad2d0eecb7923515178f9e17
James Nzomo <kazikubwa@gmail.com>
2014-06-09 16:40:51 +0300

    add billing_app to tests

--- billing_app/payments/tests.py
+++ billing_app/payments/tests.py
@@ -1,8 +1,9 @@
-from horizon.test import helpers as test  # noqa
+from openstack_dashboard.test import helpers as test  # noqa
+from django.conf import settings
 
 
 class PaymentsTests(test.TestCase):
     # Unit tests for payments.
 
     def test_me(self):
-        self.assertTrue(1 + 1 == 12)
+        self.assertTrue(1 + 1 == 2)
--- kili/local/dev_settings.py
+++ kili/local/dev_settings.py
@@ -43,45 +43,10 @@ DEBUG_TOOLBAR_PATCH_SETTINGS = False
 OPENSTACK_SSL_NO_VERIFY = True
 
 # MERCHANT SETTINGS
-HOST = getattr(settings, "HOST", "http://127.0.0.1")
-
-COMMON_INITIAL = {
-    'first_name': 'John',
-    'last_name': 'Doe',
-    'month': '06',
-    'year': '2020',
-    'card_type': 'visa',
-    'verification_value': '000'
-}
-
-GATEWAY_INITIAL = {
-    'stripe': {
-        'number': '4242424242424242',
-    },
-}
-
-INTEGRATION_INITIAL = {
-    'stripe': {
-        'amount': 1,
-        'credit_card_number': '4222222222222',
-        'credit_card_cvc': '100',
-        'credit_card_expiration_month': '01',
-        'credit_card_expiration_year': '2020'
-    },
-}
-
-for k, v in GATEWAY_INITIAL.iteritems():
-    v.update(COMMON_INITIAL)
-
-for k, v in INTEGRATION_INITIAL.iteritems():
-    v.update(COMMON_INITIAL)
-
 
 MERCHANT_TEST_MODE = True  # Toggle for live
 MERCHANT_SETTINGS = {
     "stripe": {
-        #"API_KEY": "sk_test_hXpCT9Oqs4PszDOhXK6TY9XS",
-        #"PUBLISHABLE_KEY": "pk_test_1dOtVgHQSGxzXYHk2ghg5YI1",
         "API_KEY": "sk_test_B6lIS1GVOAL8SenO3DeLlCQN",
         "PUBLISHABLE_KEY": "pk_test_GV1PYwn9wFTVQ0yHyVEWT6Ib",
     }
