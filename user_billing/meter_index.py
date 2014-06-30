20b957bc83528310a333817c8015c992155628f7
replay <mauro.stettler@gmail.com>
2014-06-30 11:05:13 +0300

    bill for previous instead of current month

--- user_billing/meter_index.py
+++ user_billing/meter_index.py
@@ -26,7 +26,7 @@ class StatisticsIndexBuilder(object):
             last_month = (datetime.datetime.utcnow().replace(day=1)
                           - datetime.timedelta(days=1))
             self.month = {'year': last_month.year,
-                          'month': last_month.month + 1}
+                          'month': last_month.month}
         return self.month
 
     def _list_billable_resource_type_meters(self):
