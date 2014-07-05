89f1bf85e077a7abc94e022c59a1e9f6f2b4c03a
replay <mauro.stettler@gmail.com>
2014-07-05 12:02:55 +0300

    add additional check before granting promotion

--- customizations/dashboards/admin/projects/forms.py
+++ customizations/dashboards/admin/projects/forms.py
@@ -8,15 +8,14 @@ class GrantPromotionForm(forms.SelfHandlingForm):
     message = forms.CharField(label='Message to User')
     project_id = forms.CharField(label='project_id', widget=forms.HiddenInput)
 
-    def __init__(self, request, *args, **kwargs):
-        super(GrantPromotionForm, self).__init__(request, *args, **kwargs)
-
     def clean_amount(self):
         if not self.cleaned_data['amount'].isnumeric():
             raise forms.ValidationError('Amount must be numeric')
         return int(self.cleaned_data['amount'])
 
     def handle(self, request, data):
+        if not request.user.is_superuser:
+            exceptions.handle(request, u'must be super user')
         try:
             transactions.UserTransactions().grant_user_promotion(
                 data['project_id'],
