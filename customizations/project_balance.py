85a4c7e762a5c71d10d1321289ab8cebce01114b
replay <mauro.stettler@gmail.com>
2014-07-07 10:53:25 +0300

    remove unnecessary ()

--- customizations/project_balance.py
+++ customizations/project_balance.py
@@ -5,5 +5,5 @@ from openstack_dashboard.dashboards.admin.projects import urls as project_urls
 class ProjectTableCustomizer:
 
     def execute(self):
-        project_urls.urlpatterns = (custom_urls.urlpatterns +
-                                    project_urls.urlpatterns)
+        project_urls.urlpatterns = custom_urls.urlpatterns + \
+            project_urls.urlpatterns
