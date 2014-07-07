af2ab0b4bd705f934dbc3df78271904368256fd1
replay <mauro.stettler@gmail.com>
2014-07-07 10:17:37 +0300

    fix amount field type for grant promo

--- customizations/dashboards/admin/projects/forms.py
+++ customizations/dashboards/admin/projects/forms.py
@@ -4,14 +4,9 @@ from horizon import forms
 
 
 class GrantPromotionForm(forms.SelfHandlingForm):
-    amount = forms.CharField(label='Amount')
+    amount = forms.DecimalField(label='Amount')
     message = forms.CharField(label='Message to User')
     project_id = forms.CharField(label='project_id', widget=forms.HiddenInput)
-
-    def clean_amount(self):
-        if not self.cleaned_data['amount'].isnumeric():
-            raise forms.ValidationError('Amount must be numeric')
-        return int(self.cleaned_data['amount'])
 
     def handle(self, request, data):
         if not request.user.is_superuser:
