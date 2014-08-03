be8c0f2c95a15ad03cc1be01afcb0b80bb1382c0
James Nzomo <kazikubwa@gmail.com>
2014-08-04 02:32:21 +0300

    Move billing templates to correct locn & fix typos

--- billing_app/payments/tables.py
+++ billing_app/payments/tables.py
@@ -11,18 +11,18 @@ LOG = logging.getLogger(__name__)
 
 class AddCard(tables.LinkAction):
 
-    name = "Add Card"
-    verbose_name = _("Add Credit / Debit Card")
-    url = "horizon:billing:payments:add_card"
+    name = 'Add Card'
+    verbose_name = 'Add Credit / Debit Card'
+    url = 'horizon:billing:payments:add_card'
     classes = ('btn-create', 'ajax-modal')
     ajax = True
 
 
 class AddFunds(tables.LinkAction):
 
-    name = "Add Funds"
-    verbose_name = _("Add Funds to Your Account")
-    url = "horizon:billing:payments:add_funds"
+    name = 'Add Funds'
+    verbose_name = 'Add Funds to Your Account'
+    url = 'horizon:billing:payments:add_funds'
     classes = ('btn-success', 'btn-large', 'ajax-modal')
     ajax = True
 
@@ -31,13 +31,13 @@ class AddFunds(tables.LinkAction):
 
 
 class DeleteCard(tables.DeleteAction):
-    name = "DeleteCard"
-    verbose_name = _("Delete Credit/Debit Card")
+    name = 'DeleteCard'
+    verbose_name = _('Delete Credit/Debit Card')
     ajax = True
-    action_present = _("Delete")
-    action_past = _("Deleted card")
-    data_type_singular = _("Card")
-    data_type_plural = _("Cards")
+    action_present = _('Delete')
+    action_past = _('Deleted card')
+    data_type_singular = _('Card')
+    data_type_plural = _('Cards')
     classes = ('btn-danger', 'btn-terminate')
 
     def delete(self, request, card_id):
@@ -45,14 +45,14 @@ class DeleteCard(tables.DeleteAction):
 
 
 class MakeDefault(tables.BatchAction):
-    name = "Make Default"
-    verbose_name = _("Make this my default card")
+    name = 'MakeDefault'
+    verbose_name = _('Make this my default card')
     ajax = True
-    action_present = _("Set as Default")
-    action_past = _("New Default")
-    data_type_singular = _("Card")
-    data_type_plural = _("Cards")
-    classes = ("btn-enable",)
+    action_present = _('Set as Default')
+    action_past = _('New Default')
+    data_type_singular = _('Card')
+    data_type_plural = _('Cards')
+    classes = ('btn-enable',)
 
     def action(self, request, obj_id):
         models.Card.objects.get(pk=obj_id).make_default()
@@ -60,15 +60,14 @@ class MakeDefault(tables.BatchAction):
 
 class StripeCardCustomerTable(tables.DataTable):
 
-    name = tables.Column("name",
-                         verbose_name=_("Card"))
-    default = tables.Column("default",
-                            verbose_name=_("Default"))
+    name = tables.Column('name',
+                         verbose_name=_('Card'))
+    default = tables.Column('default',
+                            verbose_name=_('Default'))
 
     class Meta:
-        name = "cards"
-        #verbose_name = _("Your Credit/Debit Cards")
-        verbose_name = " "
+        name = 'cards'
+        verbose_name = ' '
         table_actions = (AddFunds, AddCard)
         row_actions = (DeleteCard, MakeDefault)
 
@@ -76,30 +75,30 @@ class StripeCardCustomerTable(tables.DataTable):
 # mobile money
 class AddMobileMoneyNumber(tables.LinkAction):
 
-    name = "Add Number"
-    verbose_name = _("Add M-Pesa Enabled Number")
-    url = "horizon:billing:payments:add_number"
+    name = 'Add Number'
+    verbose_name = _('Add M-Pesa Enabled Number')
+    url = 'horizon:billing:payments:add_number'
     classes = ('btn-create', 'ajax-modal')
     ajax = True
 
 
 class EnterCode(tables.LinkAction):
 
-    name = "Enter Transaction Code"
-    verbose_name = _("Add funds via Transaction Code")
-    url = "horizon:billing:payments:enter_transaction_code"
+    name = 'Enter Transaction Code'
+    verbose_name = _('Add funds via Transaction Code')
+    url = 'horizon:billing:payments:enter_transaction_code'
     classes = ('btn-success', 'ajax-modal')
     ajax = True
 
 
 class DeleteMobileMoneyNumber(tables.DeleteAction):
-    name = "DeleteNumber"
-    verbose_name = _("Delete")
+    name = 'DeleteNumber'
+    verbose_name = _('Delete')
     ajax = True
-    action_present = _("Delete")
-    action_past = _("Deleted")
-    data_type_singular = _("Number")
-    data_type_plural = _("Nubers")
+    action_present = _('Delete')
+    action_past = _('Deleted')
+    data_type_singular = _('Number')
+    data_type_plural = _('Nubers')
     classes = ('btn-danger', 'btn-terminate')
 
     def delete(self, request, number_id):
@@ -116,12 +115,11 @@ class DeleteMobileMoneyNumber(tables.DeleteAction):
 
 class MobileMoneyTable(tables.DataTable):
 
-    name = tables.Column("number",
-                         verbose_name=_("Number"))
+    name = tables.Column('number',
+                         verbose_name=_('Number'))
 
     class Meta:
-        name = "mobile_money"
-        #verbose_name = _("Your Mobile Money Numbers")
-        verbose_name = " "
+        name = 'mobile_money'
+        verbose_name = ' '
         table_actions = (AddMobileMoneyNumber, EnterCode)
         row_actions = (DeleteMobileMoneyNumber, )
--- billing_app/payments/views.py
+++ billing_app/payments/views.py
@@ -5,9 +5,13 @@ from billing_app.models import Card  # noqa
 from billing_app.models import MobileMoneyNumber  # noqa
 from billing_app.payments import forms as payment_forms  # noqa
 from billing_app.payments import tables as payment_tables  # noqa
+from django import template
 from django.conf import settings
 from django.core import urlresolvers
 # from django.views import generic
+from django.template.defaultfilters import linebreaks  # noqa
+from django.template.defaultfilters import safe  # noqa
+from django.utils.encoding import force_unicode
 from django.utils.translation import ugettext_lazy as _
 from django.views.decorators.csrf import csrf_exempt  # noqa
 from django.views.generic.edit import FormView  # noqa
@@ -38,12 +42,8 @@ class CardTableEntry(object):
         self.default = default
 
 
-class PaymentViewBase(horizon_forms.ModalFormView):
-    success_url = urlresolvers.reverse_lazy('horizon:billing:payments:index')
-
-
 class IndexView(horizon_tables.MultiTableView):
-    template_name = 'billing_app/payments/index.html'
+    template_name = 'billing/payments/index.html'
     table_classes = (payment_tables.StripeCardCustomerTable,
                      payment_tables.MobileMoneyTable)
 
@@ -83,9 +83,39 @@ class IndexView(horizon_tables.MultiTableView):
         return cards
 
 
+class PaymentViewBase(horizon_forms.ModalFormView):
+    success_url = urlresolvers.reverse_lazy('horizon:billing:payments:index')
+    template_name = 'billing/payments/payment_form_base.html'
+    modal_header = 'Form'
+    form_submit_url = None 
+    submit_button_label = 'Submit'
+    help_text_template = None
+    help_text = ' '
+
+    def get_context_data(self, **kwargs):
+        context = super(PaymentViewBase, self).get_context_data(**kwargs)
+        context['form_submit_url'] = self.form_submit_url \
+            or urlresolvers.resolve(self.request.path_info).view_name
+        context['submit_button_label'] = self.submit_button_label
+        context['help_text'] = self.get_help_text()
+        context['modal_header'] = self.modal_header
+        return context
+
+    def get_help_text(self):
+        help_text = ' '
+        if self.help_text_template:
+            tmpl = template.loader.get_template(self.help_text_template)
+            context = template.RequestContext(self.request)
+            help_text += tmpl.render(context)
+        else:
+            help_text += linebreaks(force_unicode(self.help_text))
+        return safe(help_text)
+
+
 class AddCardView(PaymentViewBase):
     form_class = payment_forms.AddCardForm
-    template_name = "billing_app/payments/add_card.html"
+    template_name = 'billing/payments/add_card.html'
+    modal_header = 'Add Debit/Credit Card' 
 
     def get_context_data(self, **kwargs):
         context = super(AddCardView, self).get_context_data(**kwargs)
@@ -95,30 +125,31 @@ class AddCardView(PaymentViewBase):
 
 class CardPayView(PaymentViewBase):
     form_class = payment_forms.CardPayForm
-    template_name = "billing_app/payments/card_pay.html"
+    help_text_template = 'billing/payments/_card_pay_help.html'
+    modal_header = 'Add Funds'
 
     def get_context_data(self, **kwargs):
         context = super(CardPayView, self).get_context_data(**kwargs)
-        context['stripe_obj'] = billing.get_integration('stripe')
         return context
 
 
 class AddMobileNumberView(PaymentViewBase):
     form_class = payment_forms.AddMobileNumberForm
-    template_name = "billing_app/payments/add_number.html"
+    help_text_template = 'billing/payments/_add_number_help.html'
+    modal_header = 'Add Mobile Number'
 
     def get_context_data(self, **kwargs):
         context = super(AddMobileNumberView, self).get_context_data(**kwargs)
-        context['stripe_obj'] = stripe_obj
         return context
 
 
 class EnterTransactionCodeView(PaymentViewBase):
     form_class = payment_forms.MobileTransactionCodeForm
-    template_name = "billing_app/payments/mobile_transaction_code_entry.html"
+    help_text_template = \
+        'billing/payments/_mobile_transaction_code_entry_help.html'
+    modal_header = 'Enter Transaction Code'
 
     def get_context_data(self, **kwargs):
         context = super(
             EnterTransactionCodeView, self).get_context_data(**kwargs)
-        context['stripe_obj'] = stripe_obj
         return context
