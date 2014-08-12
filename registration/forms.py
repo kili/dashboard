b6bf4185b8f1afa6317f84fb3df8a945cdd29bdd
James Nzomo <kazikubwa@gmail.com>
2014-08-12 17:18:50 +0300

    Fix for #33, #36, #41
    Bump up allauth in requirements.txt
    Override allauth forms to fix 'Email' string inconsistency
    Provide alternate conclusion text in password reset email

--- registration/forms.py
+++ registration/forms.py
@@ -1,27 +1,40 @@
-from django import forms
+from allauth.account import forms as allauth_forms
+from django import forms as django_forms
 from django.utils.translation import ugettext_lazy as _
 from registration.models import User
 
 
-class RegistrationForm(forms.Form):
+class RegistrationForm(allauth_forms.SignupForm):
 
-    company = forms.CharField(label=_("Company"),
-                              min_length=5,
-                              widget=forms.TextInput(
-                                  attrs={'placeholder': _('Company Name'),
-                                         'autofocus': 'autofocus'}))
+    company = django_forms.CharField(label=_("Company"),
+        min_length=5,
+        widget=django_forms.TextInput(
+            attrs={'placeholder': _('Company Name'),
+               'autofocus': 'autofocus'}))
 
     def __init__(self, *args, **kwargs):
         super(RegistrationForm, self).__init__(*args, **kwargs)
+        email_label = _('Email')
+        self.fields["email"].label = email_label
+        self.fields["email"].widget.attrs.update({'placeholder': email_label})
 
     def clean_company(self):
         company = self.cleaned_data["company"]
         try:
             User.objects.get(company=company)
-            raise forms.ValidationError("The company name you provided"
-                                        " is already in use.")
+            raise django_forms.ValidationError(
+                "The company name you provided is already in use.")
         except User.DoesNotExist:
             return company
 
     def save(self, user):
         pass
+
+
+class ResetPasswordForm(allauth_forms.ResetPasswordForm):
+
+    def __init__(self, *args, **kwargs):
+        super(ResetPasswordForm, self).__init__(*args, **kwargs)
+        email_label = _('Email')
+        self.fields["email"].label = email_label
+        self.fields["email"].widget.attrs.update({'placeholder': email_label})
