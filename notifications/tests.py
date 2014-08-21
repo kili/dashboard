a6c5a03bd809023fce07e3b36b91817abb113ea8
replay <mauro.stettler@gmail.com>
2014-08-21 15:09:08 +0200

    added test to simulate required scenario

--- notifications/tests.py
+++ notifications/tests.py
@@ -9,37 +9,38 @@ from keystone_wrapper.client import KeystoneClient
 from accounting.transactions import UserTransactions
 
 
+def get_stub_keystone_client(number_recipients):
+
+    class StubObject(object):
+        pass
+
+    def return_tenant(arg1):
+        def return_user_list():
+            def create_user_object(num):
+                ob = StubObject()
+                setattr(ob, 'email', 'email{0}'.format(num))
+                return ob
+            return [create_user_object(i)
+                    for i in range(0, number_recipients)]
+        stub_tenant = StubObject()
+        setattr(stub_tenant, 'list_users', return_user_list)
+        return stub_tenant
+
+    stub_tenants = StubObject()
+    setattr(stub_tenants, 'get', return_tenant)
+    stub_client = StubObject()
+    setattr(stub_client, 'tenants', stub_tenants)
+    return stub_client
+
+
 class NotificationsTests(test.TestCase):
-
-    def _get_stub_keystone_client(self, number_recipients):
-
-        class StubObject(object):
-            pass
-
-        def return_tenant(arg1):
-            def return_user_list():
-                def create_user_object(num):
-                    ob = StubObject()
-                    setattr(ob, 'email', 'email{0}'.format(num))
-                    return ob
-                return [create_user_object(i)
-                        for i in range(0, number_recipients)]
-            stub_tenant = StubObject()
-            setattr(stub_tenant, 'list_users', return_user_list)
-            return stub_tenant
-
-        stub_tenants = StubObject()
-        setattr(stub_tenants, 'get', return_tenant)
-        stub_client = StubObject()
-        setattr(stub_client, 'tenants', stub_tenants)
-        return stub_client
 
     @test.create_stubs({KeystoneClient: ('get_client',)})
     def test_notification_sending(self):
         Notifications.sender_instances = {}
         number_recipients = 5
         KeystoneClient.get_client().AndReturn(
-            self._get_stub_keystone_client(number_recipients))
+            get_stub_keystone_client(number_recipients))
         self.mox.ReplayAll()
         Notifications.get_notification_sender('low_balance').add(
             project_id=1,
@@ -64,7 +65,7 @@ class NotificationsTests(test.TestCase):
             down=True)
         number_recipients = 1
         KeystoneClient.get_client().AndReturn(
-            self._get_stub_keystone_client(number_recipients))
+            get_stub_keystone_client(number_recipients))
         self.mox.ReplayAll()
         ut = UserTransactions()
         ut.receive_user_payment(1, 'STRIPE', 6, 'tester paid')
--- thresholds/tests.py
+++ thresholds/tests.py
@@ -1,9 +1,14 @@
 import pickle
+from django.core import mail
 from django.utils import timezone
 from openstack_dashboard.test import helpers as test
 from accounting.transactions import UserTransactions
 from novaclient.v1_1.servers import ServerManager
 from novaclient.v1_1.servers import Server
+from keystone_wrapper.client import KeystoneClient
+from notifications.tests import get_stub_keystone_client
+from notifications.notification_sender import Notifications
+from thresholds.models import PassedThreshold
 from thresholds.models import Threshold
 from thresholds.event_handlers import StopProjectInstances
 from thresholds.balance_thresholds import ActionQueueProcessor
@@ -13,7 +18,8 @@ other_tenant_id = 2
 instance_list = [
     Server(None, {'name': 'ser1', 'tenant_id': my_tenant_id}),
     Server(None, {'name': 'ser2', 'tenant_id': other_tenant_id}),
-    Server(None, {'name': 'ser3', 'tenant_id': my_tenant_id})]
+    Server(None, {'name': 'ser3', 'tenant_id': my_tenant_id}),
+    Server(None, {'name': 'ser4', 'tenant_id': other_tenant_id})]
 
 
 class ThresholdTests(test.TestCase):
@@ -68,4 +74,55 @@ class ThresholdTests(test.TestCase):
         ActionQueueProcessor.process()
         ActionQueueProcessor.process()
         ActionQueueProcessor.process()
+        ActionQueueProcessor.process()
+
+    @test.create_stubs({ServerManager: ('list',),
+                        KeystoneClient: ('get_client',),
+                        timezone: ('now',),
+                        instance_list[0]: ('stop',),
+                        instance_list[2]: ('stop',)})
+    def test_notification_sender_and_instance_stopper_combination(self):
+        Threshold.objects.create(
+            balance=0,
+            actions=pickle.dumps(['send_notification']),
+            up=False,
+            down=True)
+        Threshold.objects.create(
+            balance=-50,
+            actions=pickle.dumps(['send_notification',
+                                  'stop_project_instances']),
+            up=False,
+            down=True)
+        KeystoneClient.get_client().AndReturn(
+            get_stub_keystone_client(3))
+        KeystoneClient.get_client().AndReturn(
+            get_stub_keystone_client(3))
+        ServerManager.list().AndReturn(instance_list)
+        instance_list[0].stop()
+        instance_list[2].stop()
+        timezone.now().AndReturn(self.real_now)  # called by user transaction
+        timezone.now().AndReturn(self.real_now)  # called by user transaction
+        timezone.now().AndReturn(self.real_now)  # called by user transaction
+        timezone.now().AndReturn(
+            self.real_now + timezone.timedelta(seconds=2 * 24 * 60 * 60))
+        timezone.now().AndReturn(
+            self.real_now + timezone.timedelta(seconds=3 * 24 * 60 * 60))
+        timezone.now().AndReturn(
+            self.real_now + timezone.timedelta(seconds=5 * 24 * 60 * 60))
+        timezone.now().AndReturn(
+            self.real_now + timezone.timedelta(seconds=6 * 24 * 60 * 60))
+        self.mox.ReplayAll()
+        ut = UserTransactions()
+        ut.receive_user_payment(1, 'STRIPE', 6, 'user paid something')
+        ut.consume_user_money(1, 100, 'lot of consumption')
+        Notifications.send_all_notifications()
+        self.assertEqual(len(mail.outbox), 2)
+        self.assertEqual(PassedThreshold.objects.count(), 2)
+        ActionQueueProcessor.process()
+        ActionQueueProcessor.process()
+        self.assertEqual(len(instance_list[0].stop._expected_calls_queue), 1)
+        self.assertEqual(len(instance_list[2].stop._expected_calls_queue), 1)
+        ActionQueueProcessor.process()
+        self.assertEqual(len(instance_list[0].stop._expected_calls_queue), 0)
+        self.assertEqual(len(instance_list[2].stop._expected_calls_queue), 0)
         ActionQueueProcessor.process()
